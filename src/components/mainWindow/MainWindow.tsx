948e52194a26a2fa9b724f65f43eb0e9628446ce
Dima <55377558+BogDoti228@users.noreply.github.com>
2022-03-24 14:49:42 +0500

    post + get name

--- src/components/mainWindow/MainWindow.tsx
+++ src/components/mainWindow/MainWindow.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "./header/Header";
 import Content from "./content/Content";
 import Footer from "./footer/Footer";
+import {useTypeDispatch} from "../../store/store";
+import {getName} from "../../store/web-slices/profile_slice";
 
-const MainWindow : React.FC= () => {
+const MainWindow : React.FC = () => {
+    const dispatch = useTypeDispatch();
+
+    useEffect(() => {
+        dispatch(getName())
+    }, [])
+
     return (
         <>
             <Header />
--- src/store/web-slices/profile_slice.ts
+++ src/store/web-slices/profile_slice.ts
@@ -11,10 +11,20 @@ const initialState = {
 } as ProfileType
 
 export const getName = createAsyncThunk("getName", async () => {
-    const response : Promise<ProfileType> = fetch('http://localhost:8080/users/campaigns')
+    const response : Promise<ProfileType> = fetch('http://localhost:8080/user/profile')
         .then((x) => x.json())
         .catch(console.log)
     return await response
+})
+
+export const postName = createAsyncThunk("postName", async (name : string) => {
+    await fetch('http://localhost:8080/user/profile', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({name: name})
+    })
 })
 
 export const profileSlice = createSlice({
