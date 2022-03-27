693e02c857479260e460c4ea30e725b8cf40e49c
Nejjer <artemijv65@gmail.com>
2022-03-27 13:29:47 +0500

    minor commit

--- src/components/enterWindow/Enter.tsx
+++ src/components/enterWindow/Enter.tsx
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
-import {useTypeDispatch} from "../../store/store";
-import {postName, setName} from "../../store/web-slices/profile_slice";
-import {json} from "stream/consumers";
+import React, { useState } from "react";
+import { useTypeDispatch } from "../../store/store";
+import { postName, setName } from "../../store/web-slices/profile_slice";
+import { json } from "stream/consumers";
 
-function Enter({setOpen}: propsEnterWindow){
+function Enter({ setOpen }: propsEnterWindow) {
     const [nick, setNick] = useState('');
     const [isErrorMaxLen, setIsErrorMaxLenMaxLen] = useState(false);
     const [isErrorZeroInput, setIsErrorZeroInput] = useState(false);
@@ -11,24 +11,24 @@ function Enter({setOpen}: propsEnterWindow){
 
     const onChangeInput = (value: string) => {
         setIsErrorZeroInput(false);
-        if (value.length > 15){
+        if (value.length > 15) {
             setIsErrorMaxLenMaxLen(true);
-        }else {
+        } else {
             setIsErrorMaxLenMaxLen(false);
             setNick(value);
         }
     }
 
     const handleClickBtn = () => {
-        if (nick.length === 0){
+        if (nick.length === 0) {
             setIsErrorZeroInput(true);
             return;
         }
-        setOpen(v => !v );
+        setOpen(v => !v);
         dispatch(postName(nick))
     }
 
-    const handlePressEnter = (e:  React.KeyboardEvent<HTMLInputElement>) => {
+    const handlePressEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter")
             handleClickBtn();
     }
@@ -53,11 +53,11 @@ function Enter({setOpen}: propsEnterWindow){
             {isErrorMaxLen && <div className="enter-window__error-message">Слишком длинный ник</div>}
             {isErrorZeroInput && <div className="enter-window__error-message">Пустой ник</div>}
             <input placeholder="Введите свой ник"
-                   className="enter-window__input"
-                   type="text"
-                   value={nick}
-                   onChange={(e) => {onChangeInput(e.target.value)}}
-                   onKeyPress={e => {handlePressEnter(e)}}
+                className="enter-window__input"
+                type="text"
+                value={nick}
+                onChange={(e) => { onChangeInput(e.target.value) }}
+                onKeyPress={e => { handlePressEnter(e) }}
             />
             <button className="enter-window__btn btn" onClick={handleClickBtn}>Войти</button>
             <button className="enter-window__btn btn" onClick={post}>POST</button>
