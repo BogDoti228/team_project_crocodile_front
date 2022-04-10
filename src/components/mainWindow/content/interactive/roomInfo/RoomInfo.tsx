a1a0e8f4e7aa2c804a133af77c466f1be9f521fa
Nejjer <artemijv65@gmail.com>
2022-04-10 15:57:35 +0500

    Easy to merger

--- src/components/mainWindow/content/interactive/roomInfo/RoomInfo.tsx
+++ src/components/mainWindow/content/interactive/roomInfo/RoomInfo.tsx
@@ -1,34 +1,11 @@
 import React, {useEffect} from "react";
-import {RootState, useTypeDispatch} from "../../../../../store/store";
-import {getUsersList} from "../../../../../store/web-slices/list_users_slice";
-import {useSelector} from "react-redux";
-import style from "./roomInfo.module.scss";
-import {ROOM_ID_IN_STORAGE} from "../../../../../store/web-slices/chat_slice";
-import {exec} from "child_process";
+
 
 const RoomInfo : React.FC = () => {
-    const dispatch = useTypeDispatch();
-    const {usersList} = useSelector((state : RootState) => state.usersListReducer)
 
-    useEffect(() => {
-        dispatch(getUsersList())
-
-        /*setInterval(() => {
-            dispatch(getUsersList())
-        }, 1000)*/
-    },[])
-
-    const handleCopyToClipboard = () => {
-        let text = sessionStorage.getItem(ROOM_ID_IN_STORAGE);
-        if (text)
-            navigator.clipboard.writeText(text)
-                .catch(console.log);
-    }
 
     return (
-        <div className={style.room_info}>
-            <div className={style.box_word} onClick={handleCopyToClipboard}>ID: {sessionStorage.getItem(ROOM_ID_IN_STORAGE)}</div>
-        </div>
+<>  </>
     )
 }
 
--- src/components/mainWindow/header/Header.tsx
+++ src/components/mainWindow/header/Header.tsx
@@ -3,11 +3,19 @@ import logo from "../../../resources/images/logo.svg"
 import {useSelector} from "react-redux";
 import {RootState} from "../../../store/store";
 import style from './header.module.scss';
+import {ROOM_ID_IN_STORAGE} from "../../../store/web-slices/chat_slice";
 
 
 
 const Header : React.FC = () => {
     const {name} = useSelector((state : RootState) => state.profileReducer)
+    const handleCopyToClipboard = () => {
+        let text = sessionStorage.getItem(ROOM_ID_IN_STORAGE);
+        if (text)
+            navigator.clipboard.writeText(text)
+                .catch(console.log);
+    }
+
 
     return (
         <header className={style.header}>
@@ -15,8 +23,13 @@ const Header : React.FC = () => {
                 <img className={style.logo} src={logo} alt="crocodile"/>
                     <h1 className={style.title}>AlligatorZ</h1>
             </div>
-            <div className={style.profile}>
-                {name}
+            <div>
+                <div className={style.room_info}>
+                    <div className={style.box_word} onClick={handleCopyToClipboard}>ID: {sessionStorage.getItem(ROOM_ID_IN_STORAGE)}</div>
+                </div>
+                <div className={style.profile}>
+                    {name}
+                </div>
             </div>
         </header>
     )
