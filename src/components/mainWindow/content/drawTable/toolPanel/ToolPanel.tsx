7a0915b50587d06c7149941b2f4746acb9129552
Nejjer <artemijv65@gmail.com>
2022-04-12 21:58:35 +0500

    added tool panel: choice size, color and button to clear canvas

---
+++ src/components/mainWindow/content/drawTable/toolPanel/ToolPanel.tsx
@@ -0,0 +1,68 @@
+import React, {useState} from "react";
+import style from "./toolPanel.module.scss";
+import "./toolPanel.scss"
+
+type ToolPanelPropsType = {
+    activeSize: number,
+    setSize:  React.Dispatch<React.SetStateAction<number>>,
+    setColor: React.Dispatch<React.SetStateAction<string>>,
+    activeColor: string,
+    clear(): void,
+}
+
+const ToolPanel : React.FC<ToolPanelPropsType> = ({setSize, clear, activeSize, setColor, activeColor}) => {
+    const handleSetSize = (size: number) => {
+        setSize(size);
+    }
+
+    const sizeList = [
+        35,
+        25,
+        15,
+        10,
+        5,
+    ];
+
+    const colorList = [
+        '#000000',
+        '#ee3838',
+        '#00CF0AFF',
+        '#1472ff',
+        '#88301c',
+        '#FFBF3AED',
+    ]
+
+    const handleSetColor = (color: string) => {
+        setColor(color);
+    }
+
+    const isActive = (size: number | string) => {
+        if (size === activeSize || size === activeColor)
+            return style.active;
+        return null;
+    }
+
+    return (<div className={style.panel}>
+        <div className={`${style.sizeList} ${style.list}`}>
+            {sizeList.map((size) => {
+                return <div className={`${style.item} ${isActive(size)}`} onClick={() => handleSetSize(size)}><div className={`size${size}`}/></div>
+            })}
+        </div>
+        <div className={style.clearButton} onClick={clear}>
+            Очистить
+        </div>
+        <div className={`${style.colorList} ${style.list}`}>
+            <div className={`${style.color} ${isActive("#FBFBFB")}`} onClick={() => handleSetColor("#FBFBFB")}>
+                <div style={{backgroundColor: "#FBFBFB", border: "2px solid #9B9B9BFF"}}/>
+            </div>
+            {colorList.map((color) => {
+                return <div className={`${style.color} ${isActive(color)}`} onClick={() => handleSetColor(color)}>
+                    <div style={{backgroundColor: color}}/>
+                </div>
+            })}
+        </div>
+
+    </div>)
+}
+
+export default ToolPanel;
--- src/components/mainWindow/content/interactive/roomInfo/RoomInfo.tsx
+++ src/components/mainWindow/content/interactive/roomInfo/RoomInfo.tsx
@@ -1,11 +1,9 @@
-import React, {useEffect} from "react";
-
+import React from "react";
+import style from "./roomInfo.module.scss"
 
 const RoomInfo : React.FC = () => {
-
-
     return (
-<>  </>
+        <div className={style.room_info}>  </div>
     )
 }
 
