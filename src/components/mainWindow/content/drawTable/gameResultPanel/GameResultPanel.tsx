5ff562f11aff53983be7afb8a052b7401f3aaefb
Nejjer <artemijv65@gmail.com>
2022-04-26 16:39:12 +0500

    animated hiddening gameresult

--- src/components/mainWindow/content/drawTable/gameResultPanel/GameResultPanel.tsx
+++ src/components/mainWindow/content/drawTable/gameResultPanel/GameResultPanel.tsx
@@ -9,11 +9,11 @@ import {
     setCurrentStartUser
 } from "../../../../../store/web-slices/select_slice";
 import styles from "./gameResultPanel.module.scss"
-import {getScoreToAddUsers} from "../../../../../store/web-slices/role_slice";
 
 const GameResultPanel : React.FC = () => {
     const {gameState,currentWord, scoreAddUser} = useSelector((state : RootState) => state.gameProcessReducer)
     const {currentStartUser, currentTimer, currentEndScore} = useSelector((state : RootState) => state.selectReducer)
+    const [cssOpacity, setCssOpacity] = useState<React.CSSProperties>({opacity: 1})
     const {usersList} = useSelector((state : RootState) => state.usersListReducer)
     const dispatch = useTypeDispatch();
 
@@ -29,6 +29,7 @@ const GameResultPanel : React.FC = () => {
     }, [gameState]);
 
     const onGameContinue = async () => {
+        setCssOpacity({opacity: 0})
 
         const nextUser = getNextUser()
 
@@ -52,7 +53,7 @@ const GameResultPanel : React.FC = () => {
     return (
         <>
             {showForDrawingUser &&
-            <div className={styles.boxWrapPanelResult}>
+            <div className={styles.boxWrapPanelResult} style={cssOpacity}>
                 {isWordGuessed && <p className={styles.text}>Вы смогли нарисовать слово</p>}
                 {isWordGuessed && <p className={styles.text}>Вы получаете 2 очка</p>}
                 {!isWordGuessed && <p className={styles.text}>Вы не смогли нарисовать слово</p>}
@@ -62,7 +63,7 @@ const GameResultPanel : React.FC = () => {
             }
 
             {showForGuessedUser &&
-            <div className={styles.boxWrapPanelResult}>
+            <div className={styles.boxWrapPanelResult} style={cssOpacity}>
                 <p className={styles.text}>Загаданное слово: {currentWord}</p>
                 <p className={styles.text}>Вы смогли угадать слово</p>
                 <p className={styles.text}>Вы получаете 1 очко</p>
@@ -71,7 +72,7 @@ const GameResultPanel : React.FC = () => {
             }
 
             {!showForDrawingUser && !showForGuessedUser &&
-            <div className={styles.boxWrapPanelResult}>
+            <div className={styles.boxWrapPanelResult} style={cssOpacity}>
                 <p className={styles.text}>Загаданное слово: {currentWord}</p>
                 {!isWordGuessed && <p className={styles.text}>Никто не угадал слово в этом раунде</p>}
                 {isWordGuessed && <p className={styles.text}>Слово угадал: {scoreAddUser.userGuessed}</p>}
