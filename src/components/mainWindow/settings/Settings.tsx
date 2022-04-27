bbea045c2de91b4329f1975dd8415769e67f9508
Nejjer <artemijv65@gmail.com>
2022-04-27 21:37:44 +0500

    finaly commit

--- src/components/mainWindow/settings/Settings.tsx
+++ src/components/mainWindow/settings/Settings.tsx
@@ -1,80 +1,82 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { RootState, useTypeDispatch } from "../../../store/store";
+import React, {useEffect, useState} from "react";
+import {useSelector} from "react-redux";
+import {RootState, useTypeDispatch} from "../../../store/store";
 import {
-  setCurrentEndScore,
-  setCurrentStartUser,
-  setCurrentTimer,
+    setCurrentEndScore,
+    setCurrentStartUser,
+    setCurrentTimer
 } from "../../../store/web-slices/select_slice";
 import CustomSelect from "../../utils/customSelect/CustomSelect";
-import styles from "./settings.module.scss";
-import { setSettingsShow } from "../../../store/web-slices/list_users_slice";
-import { postGameProcessInfo } from "../../../store/web-slices/game_process_slice";
+import styles from "./settings.module.scss"
+import {setSettingsShow} from "../../../store/web-slices/list_users_slice";
+import {postGameProcessInfo} from "../../../store/web-slices/game_process_slice";
 
-const Settings: React.FC = () => {
-  const { usersList, timeList, scoreList } = useSelector(
-    (state: RootState) => state.usersListReducer
-  );
+const Settings : React.FC = () => {
+    const {usersList, timeList, scoreList} = useSelector((state : RootState) => state.usersListReducer)
+    const {currentStartUser, currentTimer, currentEndScore} = useSelector((state : RootState) => state.selectReducer)
+    const [cssOpacity, setCssOpacity] = useState<React.CSSProperties>({opacity: 0})
 
-  const dispatch = useTypeDispatch();
+    useEffect(() => {
+        setCssOpacity({opacity: 1})
+    }, [])
 
-  const handleChangeValueUser = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    dispatch(setCurrentStartUser(e.target.value));
-  };
+    const dispatch = useTypeDispatch()
 
-  const handleChangeValueTimer = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    dispatch(setCurrentTimer(e.target.value));
-  };
+    const onChangeValueUser = (e : React.ChangeEvent<HTMLSelectElement>) => {
+        dispatch(setCurrentStartUser(e.target.value))
+    }
 
-  const handleChangeValueScore = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    dispatch(setCurrentEndScore(e.target.value));
-  };
+    const onChangeValueTimer = (e : React.ChangeEvent<HTMLSelectElement>) => {
+        dispatch(setCurrentTimer(e.target.value))
+    }
 
-  const onClose = () => {
-    dispatch(setSettingsShow(false));
-  };
+    const onChangeValueScore = (e : React.ChangeEvent<HTMLSelectElement>) => {
+        dispatch(setCurrentEndScore(e.target.value))
+    }
 
-  const handleStart = () => {
-    dispatch(setSettingsShow(false));
-    dispatch(postGameProcessInfo("during"));
-  };
+    const onClose = () => {
+        setCssOpacity({opacity: 0})
+        setTimeout(() => {
+            dispatch(setSettingsShow(false))
+        }, 1000)
+    }
 
-  return (
-    <ul className={styles.settings_wrap_panel}>
-      <li className={styles.li_block}>
-        <p className={styles.text}>Выбрать кто начинает игру:</p>
-        <CustomSelect
-          name={"userChoose"}
-          options={usersList.map((x) => x.name)}
-          onChangeValue={handleChangeValueUser}
-        />
-      </li>
-      <li className={styles.li_block}>
-        <p className={styles.text}>Выбрать время раунда:</p>
-        <CustomSelect
-          name={"timeChoose"}
-          options={timeList}
-          onChangeValue={handleChangeValueTimer}
-        />
-      </li>
-      <li className={styles.li_block}>
-        <p className={styles.text}>Выбрать победное количество очков:</p>
-        <CustomSelect
-          name={"scoreChoose"}
-          options={scoreList}
-          onChangeValue={handleChangeValueScore}
-        />
-      </li>
-      <li className={styles.li_block}>
-        <button className={styles.button} onClick={onClose}>
-          Вернуться
-        </button>
-        <button className={styles.button} onClick={handleStart}>
-          Начать
-        </button>
-      </li>
-    </ul>
-  );
-};
+    const handleStart = () => {
+        setCssOpacity({opacity: 0})
+        setTimeout(() => {
+            dispatch(setSettingsShow(false))
+        }, 1000)
+        dispatch(postGameProcessInfo('during'))
+    }
 
-export default Settings;
+
+    return (
+        <ul className={styles.settings_wrap_panel} style={cssOpacity}>
+            <li className={styles.li_block}>
+                <p className={styles.text}>
+                    Выбрать кто начинает игру:
+                </p>
+                <CustomSelect isSelected={value => value === currentStartUser} name={"userChoose"} options={usersList.map(x => x.name)} onChangeValue={onChangeValueUser}/>
+            </li>
+            <li className={styles.li_block}>
+                <p className={styles.text}>
+                    Выбрать время раунда:
+                </p>
+                <CustomSelect isSelected={value => value === currentTimer} name={"timeChoose"} options={timeList} onChangeValue={onChangeValueTimer}/>
+            </li>
+            <li className={styles.li_block}>
+                <p className={styles.text}>
+                    Выбрать победное количество очков:
+                </p>
+                <CustomSelect isSelected={value => value === currentEndScore} name={"scoreChoose"} options={scoreList} onChangeValue={onChangeValueScore}/>
+            </li>
+            <li className={styles.li_block}>
+                <button className={styles.button} onClick={onClose}>Вернуться</button>
+                <button className={styles.button} onClick={handleStart}>Начать</button>
+
+            </li>
+        </ul>
+    )
+}
+
+export default Settings
--- src/store/web-slices/list_users_slice.ts
+++ src/store/web-slices/list_users_slice.ts
@@ -1,75 +1,77 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { API_PATH, ROOM_ID_IN_STORAGE } from "../../constans";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {ROOM_ID_IN_STORAGE} from "./chat_slice";
+import {API_PATH} from "../../constans";
 
 export interface UserInfoType {
-  name: string;
-  score: string;
+    name : string,
+    score : string
 }
 
 interface UserListType {
-  usersList: Array<UserInfoType>;
-  timeList: Array<string>;
-  scoreList: Array<string>;
-  settingsShow: boolean;
-  loading?: "idle" | "pending" | "succeeded" | "failed";
+    usersList: Array<UserInfoType>,
+    timeList: Array<string>,
+    scoreList: Array<string>,
+    settingsShow: boolean,
+    loading?: 'idle' | 'pending' | 'succeeded' | 'failed'
 }
 
 interface ServerUserInfoType {
-  Name: string;
-  Score: string;
+    Name: string,
+    Score: string
 }
 
 const initialState = {
-  usersList: [],
-  timeList: ["00:10", "01:00", "02:00", "03:00", "04:00", "05:00"],
-  scoreList: ["5", "10", "15", "20", "25", "30"],
-  loading: "idle",
-  settingsShow: false,
-} as UserListType;
+    usersList : [],
+    timeList : ["00:10","01:00", "02:00", "03:00", "04:00", "05:00"],
+    scoreList : ["5", "10", "15", "20", "25", "30"],
+    loading: 'idle',
+    settingsShow: false,
+} as  UserListType
 
 export const getUsersList = createAsyncThunk("getUsersList", async () => {
-  const response: Promise<string> = fetch(API_PATH + "users/list", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Room-Id": sessionStorage.getItem(ROOM_ID_IN_STORAGE) as string,
-    },
-  })
-    .then((x) => {
-      if (x.ok) {
-        return x.json();
-      }
-      console.error("Error get user list with status", x.status);
-      return JSON.stringify([]);
+    const response : Promise<string> = fetch(API_PATH + 'users/list', {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Room-Id" : sessionStorage.getItem(ROOM_ID_IN_STORAGE) as string
+        }
     })
-    .catch(console.log);
-  return await response;
-});
+        .then((x) => {
+            if (x.ok){
+                return x.json();
+            }
+            console.error('Error get user list with status', x.status);
+            return JSON.stringify([]);
+        })
+        .catch(console.log)
+    return await response
+})
 
 export const usersListSlice = createSlice({
-  name: "userListSlice",
-  initialState: initialState,
-  reducers: {
-    setSettingsShow: (state, action) => {
-      state.settingsShow = action.payload;
+    name : "userListSlice",
+    initialState : initialState,
+    reducers : {
+        setSettingsShow : (state, action) => {
+            state.settingsShow = action.payload;
+        }
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(getUsersList.fulfilled, (state, action) => {
-      let userList: Array<ServerUserInfoType> = [];
+    extraReducers: (builder) => {
+        builder.addCase(getUsersList.fulfilled, (state, action) => {
+            let userList : Array<ServerUserInfoType> = [];
 
-      try {
-        userList = JSON.parse(action.payload);
-      } catch (e) {
-        console.error("Не могу распарсить JSON", action.payload);
-        console.error(e);
-      }
-      state.usersList = userList.map((x) => {
-        return { name: x.Name, score: x.Score };
-      });
-    });
-  },
-});
+            try{
+                userList = JSON.parse(action.payload);
+            }
+            catch (e)
+            {
+                console.error('Не могу распарсить JSON', action.payload)
+                console.error(e)
+            }
+            state.usersList = userList.map(x => {return {name : x.Name, score : x.Score}})
+
+        })
+    }
+})
 
 export const usersListSliceReducers = usersListSlice.reducer;
-export const { setSettingsShow } = usersListSlice.actions;
+export const {setSettingsShow} = usersListSlice.actions
